154e6270a72157a09a8f764737045b7579dafa92
unknown <kalin_gyokov@hotmail.com>
2016-01-23 23:16:53 -0800

    Initial commit

---
+++ src/finger_tree_transform.erl
@@ -0,0 +1,36 @@
+%%%-------------------------------------------------------------------
+%%% @author Kalin
+%%% @copyright (C) 2016, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 23. Jan 2016 6:09 PM
+%%%-------------------------------------------------------------------
+-module(finger_tree_transform).
+-author("Kalin").
+
+%% API
+-export([parse_transform/2, format_error/1]).
+
+-define(MOD,finger_tree_monoid@).
+
+parse_transform(Forms,_Options) ->
+    io:format("~p~n",Forms),
+    Forms.
+
+transform_monoid_ref(
+        {
+            remote,_,
+            {atom,_,?MOD},
+            {atom,_,Fun}
+        }) ->
+        {atom,_,Fun}.
+
+%%{call,113,
+%%{atom,113,
+%%to_tree},
+%%[{var,113,
+%%'SF'}]}]}
+
+format_error(_Err) -> ok.
+
